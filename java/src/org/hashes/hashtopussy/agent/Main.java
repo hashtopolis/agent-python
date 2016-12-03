3c133c33497339111113a8d63ca41de27214db7d
Sein Coray <sein.coray@gmail.com>
2016-12-03 14:38:08 +0100

    added java project

---
+++ java/src/org/hashes/hashtopussy/agent/Main.java
@@ -0,0 +1,10 @@
+package org.hashes.hashtopussy.agent;
+
+/**
+ * Created by sein on 03.12.16.
+ */
+public class Main {
+    public static void main(String[] args){
+        //entry point
+    }
+}
---
+++ java/src/org/hashes/hashtopussy/agent/request/Request.java
@@ -0,0 +1,9 @@
+package org.hashes.hashtopussy.agent.request;
+
+import org.json.*;
+
+/**
+ * Created by sein on 03.12.16.
+ */
+public class Request {
+}
