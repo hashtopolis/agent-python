900f405c6b2b7e7d3089eafb4b8a6dff970f2941
blazer <blazerx@gmail.com>
2017-07-06 20:17:37 +1000

    version to 0.43.19

--- csharp/hashtopussy/Program.cs
+++ csharp/hashtopussy/Program.cs
@@ -137,7 +137,7 @@ namespace hashtopussy
 
             }
 
-            string AppVersion = "0.43.17";
+            string AppVersion = "0.43.19";
             Console.WriteLine("Client Version " + AppVersion);
 
             initConnect();
