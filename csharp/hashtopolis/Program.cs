5661332f340695f663f598b669286553f2cb0629
blazer <blazerx@gmail.com>
2018-03-08 23:51:56 +1100

    version 0.51.9

--- csharp/hashtopolis/Program.cs
+++ csharp/hashtopolis/Program.cs
@@ -151,7 +151,7 @@ namespace hashtopolis
                 }
             }
 
-            string AppVersion = "0.51.8";
+            string AppVersion = "0.51.9";
             Console.WriteLine("Client Version " + AppVersion);
 
             initConnect();
