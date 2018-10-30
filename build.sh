01ed681207f471cb8d51e08344844758a04ddf9f
Sein Coray <s3inlc@hashes.org>
2018-10-30 11:46:21 +0100

    build script is writing commit number since release into zip

--- build.sh
+++ build.sh
@@ -3,4 +3,16 @@
 if [ -f hashtopolis.zip ]; then
   rm hashtopolis.zip
 fi
+
+# write commit count since release into version number when compiling into zip
+count=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline | wc -l)
+if [ ${count} \> 0 ];
+then
+    sed -i -E 's/return "([0-9]+)\.([0-9]+)\.([0-9]+)"/return "\1.\2.\3.'$count'"/g' htpclient/initialize.py
+fi;
 zip -r hashtopolis.zip __main__.py htpclient -x "*__pycache__*"
+if [ ${count} \> 0 ];
+then
+    ssed -i -E 's/return "([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"/return "\1.\2.\3"/g' htpclient/initialize.py
+fi;
+
