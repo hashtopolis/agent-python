5278c581c4b4772a12a4bfd0739fd7db22ef094f
Sein Coray <s3inlc@hashes.org>
2018-11-13 16:17:01 +0100

    reverted build changes

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ fi
 count=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline | wc -l)
 if [ ${count} \> 0 ];
 then
-    sed -i -E 's/return "\([0-9]+\)\.\([0-9]+\)\.\([0-9]+\)"/return "\1.\2.\3.'"$count"'"/g' htpclient/initialize.py
+    sed -i -E 's/return "([0-9]+)\.([0-9]+)\.([0-9]+)"/return "\1.\2.\3.'$count'"/g' htpclient/initialize.py
 fi;
 zip -r hashtopolis.zip __main__.py htpclient -x "*__pycache__*"
 if [ ${count} \> 0 ];
 then
-    sed -i -E 's/return "\([0-9]+\)\.\([0-9]+\)\.\([0-9]+\)\.\([0-9]+\)"/return "\1.\2.\3"/g' htpclient/initialize.py
+    sed -i -E 's/return "([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)"/return "\1.\2.\3"/g' htpclient/initialize.py
 fi;
 
