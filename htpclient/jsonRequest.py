f4936632abbab2dfc7efa1c6b2975acc086227af
Eric Fossum <eric_fossum@selinc.com>
2023-12-15 21:55:12 +0000

    Allow redirects for reverse proxy.

--- htpclient/jsonRequest.py
+++ htpclient/jsonRequest.py
@@ -1,10 +1,11 @@
 import logging
 
-from htpclient.config import *
-from htpclient.session import *
+from htpclient.config import Config
+from htpclient.session import Session
 
 
 class JsonRequest:
+
     def __init__(self, data):
         self.data = data
         self.config = Config()
@@ -17,7 +18,8 @@ class JsonRequest:
                 self.config.get_value('url'),
                 json=self.data,
                 timeout=30,
-                verify=not ignore_certificate)
+                verify=not ignore_certificate,
+                allow_redirects=True)
             if r.status_code != 200:
                 logging.error("Status code from server: " + str(r.status_code))
                 return None
