6c9f2354eb247c2382d29a72e40148ff3d0806fc
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2025-05-29 11:58:28 +0100

    Fixed typo

--- htpclient/jsonRequest.py
+++ htpclient/jsonRequest.py
@@ -13,7 +13,7 @@ class JsonRequest:
     def execute(self):
         try:
             logging.debug(self.data)
-            headers = self.config.get_value('http_headers') or {}
+            headers = self.config.get_value('http-headers') or {}
             r = self.session.post(self.config.get_value('url'), json=self.data, timeout=30, headers=headers)
             if r.status_code != 200:
                 logging.error("Status code from server: " + str(r.status_code))
