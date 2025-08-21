e2e7acb39755d12c5349a7c86cfa5c63af867ae6
sein <s3inlc@hashes.org>
2025-08-21 21:45:15 +0200

    added hashcat status test for multiple devices

--- tests/test_hashcat_status.py
+++ tests/test_hashcat_status.py
@@ -36,6 +36,23 @@ class TestHashcatStatus(unittest.TestCase):
         self.assertEqual(status.power, [56])
         self.assertEqual(status.unknown_fields, False)
 
+    def test_hashcat_7_multiple_devices(self):
+        line = "STATUS\t5\tSPEED\t8947055\t1000\t0\t1000\tEXEC_RUNTIME\t0.062464\t0.000000\tCURKU\t0\tPROGRESS\t9025\t9025\tRECHASH\t0\t1\tRECSALT\t0\t1\tTEMP\t44\t-1\tREJECTED\t0\tUTIL\t46\t-1\tPOWER\t-1\t-1\t"
+        status = HashcatStatus(line)
+        self.assertTrue(status.is_valid())
+        self.assertEqual(status.status, 5)
+        self.assertEqual(status.speed, [[8947055, 1000], [0, 1000]])
+        self.assertEqual(status.exec_runtime, [0.062464, 0.000000])
+        self.assertEqual(status.curku, 0)
+        self.assertEqual(status.progress, [9025, 9025])
+        self.assertEqual(status.rec_hash, [0, 1])
+        self.assertEqual(status.rec_salt, [0, 1])
+        self.assertEqual(status.temp, [44, -1])
+        self.assertEqual(status.rejected, 0)
+        self.assertEqual(status.util, [46, -1])
+        self.assertEqual(status.power, [-1, -1])
+        self.assertEqual(status.unknown_fields, False)
+
     def test_valid_status_line(self):
         line = "STATUS\t1\tSPEED\t2534\t1000\tEXEC_RUNTIME\t123\tCURKU\t45\tPROGRESS\t67\t100\tRECHASH\t89\t120\tRECSALT\t56\t110\tTEMP\t25\tREJECTED\t7\tUTIL\t85\t90\tPOWER\t100\t150"
         status = HashcatStatus(line)
