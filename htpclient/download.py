4e7e6cb3de63efa7baa89a564082c2f7e26f480b
jessevz <jesse.van.zutphen@nfi.nl>
2026-03-17 19:16:42 +0100

    Support redirect for head request for downloads

--- htpclient/download.py
+++ htpclient/download.py
@@ -17,7 +17,7 @@ class Download:
 
             # Check header
             if not no_header:
-                head = session.head(url)
+                head = session.head(url, allow_redirects=True)
                 # not sure if we only should allow 200/301/302, but then it's present for sure
                 if head.status_code not in [200, 301, 302]:
                     logging.error("File download header reported wrong status code: " + str(head.status_code))
